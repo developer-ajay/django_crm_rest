906260dcdb080647208001cb23e71006e6726daf
developer-ajay <developer.ajay618@gmail.com>
2025-03-23 20:10:44 +0530

    api app & classView

--- dcrm/dcrm/urls.py
+++ dcrm/dcrm/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('classView/', views.classView.as_view()),
     path('', views.home , name='home'),
 ]
--- dcrm/dcrm/views.py
+++ dcrm/dcrm/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.   
 def home(request):
     return render(request, 'index.html')
+
+class classView(TemplateView):
+    template_name = 'classView.html'
