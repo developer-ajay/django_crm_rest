6e5c146906251df0d446456c01f7fa68c50b67d7
developer-ajay <developer.ajay618@gmail.com>
2025-03-25 16:24:37 +0530

    Record model Serializer and views

--- dcrm/api/views.py
+++ dcrm/api/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from .serializers import UserSerializer
+from .models import Record
+from .serializers import RecordSerializer, UserSerializer
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny , IsAuthenticated
 # Create your views here.
 
 class userView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+class RecordView(generics.ListCreateAPIView):
+    queryset = Record.objects.all()
+    serializer_class = RecordSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return Record.objects.all()
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            return serializer.ValidationError(serializer.errors)
