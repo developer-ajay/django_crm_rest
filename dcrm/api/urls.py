eda87d635e5f3ed58392a0afa2652261fb794046
developer-ajay <developer.ajay618@gmail.com>
2025-03-25 15:41:27 +0530

    JWT views

--- dcrm/api/urls.py
+++ dcrm/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import userView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('user/register', userView.as_view(),name='register'),
+    path('auth/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
