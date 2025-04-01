dac9a1fbe0ebb3e0b8967e5a56d5f529f23be102
developer-ajay <developer.ajay618@gmail.com>
2025-04-01 21:20:12 +0530

    changes in record url

--- dcrm/api/urls.py
+++ dcrm/api/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('user/register', userCreateView.as_view(),name='register'),
     path('auth/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('record/register', RecordListView.as_view(),name='register_record'),
+    path('records/', RecordListView.as_view(),name='register_record'),
     path('record/<int:pk>', RecordRetieveUpdateDeleteView.as_view(),name='record_detail'),
 ]
