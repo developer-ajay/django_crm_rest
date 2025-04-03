8f22ea345e74db99179b35eefa6a34fc9638e452
developer-ajay <developer.ajay618@gmail.com>
2025-04-03 17:57:42 +0530

    getting access & refresh tokens

--- dcrm/static/js/index.js
+++ dcrm/static/js/index.js
@@ -1,3 +1,5 @@
 axios.post('/api/auth/token' , {username: 'ajay', password: 'Ajay@123'}).then(response => {
-    console.log(response.data)
+    console.log(response.data);
+    localStorage.setItem('access' , response.data.access);
+    localStorage.setItem('refresh' , response.data.refresh);
 })
